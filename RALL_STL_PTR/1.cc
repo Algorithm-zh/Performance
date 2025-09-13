a41976e9136e5fd77ead75563a2890e632a1c4a6
fire <24031212284>
2025-09-13 18:01:01 +0800

    主要是各种构造函数的学习

--- RALL_STL_PTR/1.cc
+++ RALL_STL_PTR/1.cc
@@ -17,6 +17,13 @@ void myFunc(auto &&v)
     std::cout << vi << std::endl;
   }
 }
+//如果一个类里全都是写基础类型(指针)组成，那这些类型被称为POD
+//编译器将不会自动生成无参的构造函数去初始化这些成员
+class Pig{
+public:
+  std::string m_name;
+  int m_weight{0};
+};
 
 int main (int argc, char *argv[]) {
 
@@ -30,5 +37,8 @@ int main (int argc, char *argv[]) {
   int sum2 = std::reduce(v.begin(), v.end(), 1, std::multiplies{});
 
   std::cout << sum << ' '  << sum2 << std::endl;
+
+  Pig pig;
+  std::cout << pig.m_name << ' ' << pig.m_weight << std::endl;
   return 0;
 }
---
+++ RALL_STL_PTR/2.cc
@@ -0,0 +1,120 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <format>
+
+struct Demo{
+  explicit Demo(std::string a, std::string b)
+  {
+    std::cout << std::format("Hello {0}, I am {1}\n", a, b);  
+  }
+};
+
+//如果一个类里全都是写基础类型(指针)组成，那这些类型被称为POD
+//编译器将不会在自动生成无参的构造函数里去初始化这些成员
+//只会去掉用那些有无参构造函数类型的构造函数
+//所以需要我们自己去指定初始化值
+class Pig{
+public:
+  std::string m_name;
+  int m_weight{};//括号里不写任何东西会默认初始化为0（指针为nullptr）(数组也会全部初始化为0)
+  Demo m_demo{"Sam", "Lily"};//编译通过
+  //Demo m_demo2 = {"Sam", "Lily"};//编译报错，使用了explicit,必须显示构造
+};
+
+
+//解决函数多返回值
+Pig pigFunc()
+{
+  return {"sam", 120};
+}
+
+//编写自己的vector类
+struct Vector{
+  size_t m_size;
+  int *m_data;
+
+  explicit Vector(size_t n)
+  {
+    m_size = n;
+    m_data = (int *)malloc(sizeof(int) * n);
+  }
+  //定义了析构函数，我们要么删除默认的拷贝构造要么自己定义一个新的
+  //因为默认的是浅拷贝，只会对指针进行拷贝，不会对深层数组进行拷贝
+  Vector(Vector const &other)
+  {
+    m_size = other.m_size;
+    m_data = (int *)malloc(m_size * sizeof(int));
+    memcpy(m_data, other.m_data, m_size * sizeof(int));
+  }
+  //拷贝赋值也是一样
+  Vector &operator=(Vector const &other)
+  {
+    m_size = other.m_size;
+    //内存的销毁重新分配通过realooc,可以利用现有的m_data,避免重新分配
+    m_data = (int *)realloc(m_data, m_size * sizeof(int));
+    memcpy(m_data, other.m_data, m_size * sizeof(int));
+    return *this;
+    //如果不考虑提高性能可以这样做 
+    // this->~Vector();//先销毁现在的
+    // new(this)Vector(other);//再重新构造
+    // return *this;
+  }
+
+  //移动构造
+  Vector(Vector &&other)
+  {
+    m_size = other.m_size;
+    other.m_size = 0;
+    m_data = other.m_data;
+    other.m_data = nullptr;
+  }
+  //移动拷贝
+  Vector &operator=(Vector &&other)
+  {
+    this->~Vector();
+    new(this)Vector(std::move(other));
+    return *this;
+  }
+
+  ~Vector()
+  {
+    free(m_data);
+  }
+  size_t size()
+  {
+    return m_size;
+  }
+
+  void resize(size_t size)
+  {
+    m_size = size;
+    m_data = (int *)realloc(m_data, m_size * sizeof(int));
+  }
+
+  int &operator[](size_t index)
+  {
+    return m_data[index];
+  }
+};
+
+int main (int argc, char *argv[]) {
+  
+  //c++11里在没有定义任何构造函数时，编译器会自动生成一个参数个数和成员一样的构造函数
+  //但是只能用{}来使用(为了兼容c++98)
+  Pig pig{"zhansan", 120, Demo{"Tom", "Jieke"}};
+  std::cout << pig.m_name << ' ' << pig.m_weight << std::endl;
+
+  Pig pig2 = std::move(pigFunc());
+  //内存交换swap只有O(1)的时间复杂度
+  std::swap(pig, pig2);
+  std::cout << pig.m_name << ' ' << pig.m_weight << std::endl;
+
+  //以下这俩基本没有区别，但是那些基础类型的话会产生随机初始化(而不是零初始化)
+  Pig* pig3 = new Pig;
+  Pig* pig4 = new Pig();
+  int* a = new int;
+  int* b = new int();
+  std::cout << *a << ' ' << *b << std::endl;
+  return 0;
+}
